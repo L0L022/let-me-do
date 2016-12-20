ef60c6ee51bb8e24d48310c6b40ca5c29044e60a
L0L022 <L0L022@openmailbox.org>
2016-12-20 15:10:06 +0100

    let_me_do.sh -> let_me_do.bash

--- install.bash
+++ install.bash
@@ -7,5 +7,5 @@ systemctl enable sshd.service
 systemctl start sshd.service
 
 mkdir -p /usr/local/bin/ /usr/local/share/applications/
-cp let_me_do.sh /usr/local/bin/let_me_do
+cp let_me_do.bash /usr/local/bin/let_me_do
 cp let_me_do.desktop /usr/local/share/applications/
